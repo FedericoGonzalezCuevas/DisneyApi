7e3ec79a690093f9d920fa059f4befaabfafd99f
FedericoGonzalezCuevas <98536849+FedericoGonzalezCuevas@users.noreply.github.com>
2022-01-28 13:45:35 -0300

    Create Genero.java

---
+++ Genero.java
@@ -0,0 +1,14 @@
+package Disney;
+
+public class Genero {
+
+    //Atributos de la clase
+    String nombre;
+    String peli_serie;
+
+    //Metodos de la clase
+    public void mostrarGenero() {
+        System.out.println("\n Nombre: " + nombre);
+        System.out.println("\n Pelicula / Serie asociada: " + peli_serie);
+    }
+}
